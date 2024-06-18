c045b867a5a18562bda55c5a3784b91c5b8518bd
Tushar Arora <tarora22@myseneca.ca>
2024-06-18 04:29:10 -0400

    V1.1

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Project-1---SignUp
-Version 1.0:
-Push date: Tuesday, June 18.
-Version Info: Got the primitive functionality of the project done, right now, the signup and login system works, and both functinslities only accept a username and password for now. The system uses library bcrypt to hash the password and jsonWebToken to create the token to return to the user. 
-What's next? The sign-up system will use Username, Email and Password, all of which will be validated for certain conditions. 
+## Project Description
+This basic Signup/Login form allows a user to create an account using their First Name, Last Name, Email and Password. <br/>
+There are many different validations that are performed on the information entered by the user. <br/>
+Once the user has successfully entered all the information, their data is stored in the database, and a unique Token is generated for thier profile. <br/>
+The login form allows a user to log into their profile using their username and password.<br/>
+## JS libraries and Frameworks Used
+* Express JS
+* BCRYPT
+* bodyParser
+* jsonwebtoken
+* mysql2
+### Version 1.1:
+Push date: Tuesday, June 18. <br/>
+Version Info: This interation of the signup and login system is able to perform all the pre-required tasks including encrypting passwords, performing validations <br/>
+and assigning a unique token to each user.<br/>
+### Version 1.0:
+Push date: Tuesday, June 18.<br/>
+Version Info: Got the primitive functionality of the project done, right now, the signup and login system works, and both functinslities <br/>only accept a username and password for now. The system uses library bcrypt to hash the password and jsonWebToken to create the token to <br/>return to the user. 
+
--- index.js
+++ index.js
@@ -27,30 +27,58 @@ const db = mysql.createConnection({
 
 
   app.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
-  
-    if (!username || !password) {
-      return res.status(400).send('Username and password are required');
+    const { firstName, lastName, username, email, password } = req.body;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+    const nameRegex = /^[A-Za-z]+$/;
+    if (!username || !password || !firstName || !lastName || !email) {
+        return res.status(400).send('Please enter all details');
+      }
+    if (!email.includes("@"))
+    {
+        return res.status(400).send('Please enter a valid email address');
+
     }
+    if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
+        return res.status(400).send('First name and last name must contain only letters');
+      }
+    
+    if (!passwordRegex.test(password)) {
+        return res.status(400).send('Password must contain at least one uppercase letter, one number, and one special character, and be at least 8 characters long');
+      }
   
+      db.query(
+        'SELECT * FROM users1 WHERE username = ?',
+        [username],
+        async (err, results) => {
+          if (err) {
+            return res.status(500).send('Error checking username uniqueness');
+          }
+    
+          if (results.length > 0) {
+            return res.status(400).send('Username is already taken');
+          }
+    
     try {
       const hashedPassword = await bcrypt.hash(password, 10);
+      const token = jwt.sign({ userId: results.insertId }, SECRET_KEY);
       db.query(
-        'INSERT INTO users (username, password) VALUES (?, ?)',
-        [username, hashedPassword],
+        'INSERT INTO users1 (firstName, lastName, username, email, password, token) VALUES (?, ?, ?, ?, ?, ?)',
+        [firstName, lastName, username, email, hashedPassword, token],
         (err, results) => {
           if (err) {
             return res.status(500).send('Error creating user');
           }
   
-          const token = jwt.sign({ userId: results.insertId }, SECRET_KEY);
+          
           res.status(201).send({ token });
         }
       );
     } catch (error) {
       res.status(500).send('Error processing request');
     }
-  });
+}
+      );
+    });
 
   
   app.post('/login', (req, res) => {
@@ -61,7 +89,7 @@ const db = mysql.createConnection({
     }
   
     db.query(
-      'SELECT * FROM users WHERE username = ?',
+      'SELECT * FROM users1 WHERE username = ?',
       [username],
       async (err, results) => {
         if (err) {
@@ -79,9 +107,7 @@ const db = mysql.createConnection({
         if (!passwordMatch) {
           return res.status(400).send('Invalid credentials');
         }
-  
-        const token = jwt.sign({ userId: user.id }, SECRET_KEY);
-        res.status(200).send({ token });
+        res.status(200).send({ message: "Welcome "  + user.firstName + "Your token is " + user.token});
       }
     );
   });
